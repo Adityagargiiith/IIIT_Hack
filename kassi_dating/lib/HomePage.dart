5e5b962d2b3cc5f0163d65c32a8a90e01f0738b1
Om Mehta <117066040+OM6978@users.noreply.github.com>
2024-01-21 15:22:17 +0530

    Navigationn

--- kassi_dating/lib/HomePage.dart
+++ kassi_dating/lib/HomePage.dart
@@ -8,14 +8,62 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
-  // static const List<Widget> _widgetOptions = <Widget>[
-  //   Text('Home Page',
-  //       style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-  //   Text('Search Page',
-  //       style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-  //   Text('Profile Page',
-  //       style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-  // ];
+  static final _widgetOptions = <Widget>[
+    Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+          Container(
+              child: CircleAvatar(
+                backgroundColor: Colors.black,
+                radius: 70,
+              ),
+              height: 220),
+          SizedBox(
+            width: 105,
+          ),
+          Text('O.M.', style: TextStyle(fontSize: 20))
+        ]),
+        SizedBox(height: 25),
+        Text('History', style: TextStyle(fontSize: 20)),
+      ],
+    ),
+    Text('Profile Page',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Container(
+      margin: EdgeInsets.only(top: 25, left: 25, right: 25),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Flexible(
+                flex: 1,
+                child: TextField(
+                  style: TextStyle(color: Colors.white),
+                  cursorColor: Colors.grey,
+                  decoration: InputDecoration(
+                      fillColor: Colors.black,
+                      filled: true,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none),
+                      hintText: 'Search',
+                      hintStyle: TextStyle(
+                          color: Color.fromARGB(255, 218, 213, 213),
+                          fontSize: 18),
+                      prefixIcon: Container(
+                        padding: EdgeInsets.all(15),
+                        child: Image.asset('images/search.png'),
+                        width: 18,
+                      )),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    ),
+  ];
 
   void _onItemTapped(int index) {
     setState(() {
@@ -39,25 +87,7 @@ class _HomePageState extends State<HomePage> {
             ],
           )),
       body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              Container(
-                  child: CircleAvatar(
-                    backgroundColor: Colors.black,
-                    radius: 70,
-                  ),
-                  height: 220),
-              SizedBox(
-                width: 105,
-              ),
-              Text('O.M.', style: TextStyle(fontSize: 20))
-            ]),
-            SizedBox(height: 25),
-            Text('History', style: TextStyle(fontSize: 20)),
-          ],
-        ),
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
