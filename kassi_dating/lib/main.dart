17f17d5343c227d2e7eeb7445e77f61dca88329b
Adityagargiiith <127509487+Adityagargiiith@users.noreply.github.com>
2024-01-21 15:20:07 +0530

    authentication

--- kassi_dating/lib/main.dart
+++ kassi_dating/lib/main.dart
@@ -1,8 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'HomePage.dart';
+import 'firebase_auth.dart';
+import 'signup.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MyApp());
 }
 
@@ -22,7 +31,15 @@ class LoginDemo extends StatefulWidget {
 }
 
 class _LoginDemoState extends State<LoginDemo> {
+  final FirebaseAuthService _auth = FirebaseAuthService();
+
+  TextEditingController _emailcontroller = TextEditingController();
+  TextEditingController _passwordcontroller = TextEditingController();
   @override
+  void dispose(){
+    _emailcontroller.dispose();
+    _passwordcontroller.dispose();
+  }
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
@@ -47,6 +64,7 @@ class _LoginDemoState extends State<LoginDemo> {
               //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
               padding: EdgeInsets.symmetric(horizontal: 15),
               child: TextField(
+                controller: _emailcontroller,
                   decoration: InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: 'Email',
@@ -58,6 +76,7 @@ class _LoginDemoState extends State<LoginDemo> {
                   left: 15.0, right: 15.0, top: 15, bottom: 0),
               //padding: EdgeInsets.symmetric(horizontal: 15),
               child: TextField(
+                controller: _passwordcontroller,
                   obscureText: true,
                   decoration: InputDecoration(
                       border: OutlineInputBorder(),
@@ -79,10 +98,9 @@ class _LoginDemoState extends State<LoginDemo> {
               width: 250,
               decoration: BoxDecoration(
                   color: Colors.blue, borderRadius: BorderRadius.circular(20)),
-              child: TextButton(
+                child: TextButton(
                 onPressed: () {
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (_) => HomePage()));
+                  signin();
                 },
                 child: Text(
                   'Login',
@@ -93,13 +111,49 @@ class _LoginDemoState extends State<LoginDemo> {
             SizedBox(
               height: 130,
             ),
-            Text(
-              'New User? Create Account',
-              style: TextStyle(color: Colors.white),
+            GestureDetector(
+              onTap: () {
+                navigateToSignupPage(); // Call the method to navigate to the signup page
+              },
+              child: Text(
+                'New User? Create Account',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
           ],
         ),
       ),
     );
+  }
+  void navigateToSignupPage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SignupPage()),
+    );
+  }
+
+  void signin() async {
+      String email = _emailcontroller.text;
+      String password = _passwordcontroller.text;
+
+      User? user = await _auth.signinwithemailandpassword(email, password);
+      if(user != null){
+        print("user logged in");
+
+        Navigator.push(
+            context, MaterialPageRoute(builder: (_) => HomePage()));
+
+      }
+      else{
+        print("Error during sign-in");
+      }
+
+      // Sign-in successful, you can add further logic here if needed
+
+      // Handle authentication errors
+
+
+      // You can show an error message to the user or perform other actions based on the error
+
   }
 }
